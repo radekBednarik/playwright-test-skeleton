b51e29deb669c1cd62ba44c939e95e5456d2cacb
radek.Bednarik <radek.bednarik@tesena.com>
2024-04-04 11:44:49 +0200

    dev: added coloring via chalk

--- bin/playwright-skeleton-ts.js
+++ bin/playwright-skeleton-ts.js
@@ -4,37 +4,45 @@ const { join } = require("path");
 const { execSync } = require("child_process");
 const { platform } = require("os");
 const { copy } = require("fs-extra");
+const chalk = require("chalk");
 
 const projectDir = process.cwd();
 const templateDir = join(__dirname, "../templates/ts");
 
-console.log("Creating skeleton project...");
+console.log(chalk.blue("Creating skeleton project...\n"));
 
 copy(templateDir, projectDir, (err) => {
   if (err)
     throw Error(
-      `Copying from ${templateDir} to ${projectDir} failed with error: ${err}`
+      chalk.red(
+        `Copying from ${templateDir} to ${projectDir} failed with error: ${err}`
+      )
     );
 
   const opts = { cwd: projectDir };
 
   // init git repo with branch name as main
+  console.log(chalk.green("Setting up git repo"));
   execSync("git config init.defaultBranch main && git init", opts);
 
   // install deps
+  console.log(chalk.green("Running npm install"));
   execSync("npm install", opts);
-  console.log("basic deps installed");
 
   // create default precommit hooks
+  console.log(chalk.green("Setting up precommit hooks"));
   execSync("npx mrm@2 lint-staged", opts);
-  console.log("default precommit hooks created");
 
   // finish playwright setup
   if (platform === "linux") {
+    console.log(
+      chalk.green("Linux platform detected, setting up needed linux packages")
+    );
     execSync("npx playwright install-deps", opts);
   }
+
+  console.log(chalk.green("Installing Playwright browser binaries"));
   execSync("npx playwright install", opts);
-  console.log("Playwright setup finished");
 });
 
-console.log("Skeleton project created");
+console.log(chalk.blue("\nSkeleton project created"));
