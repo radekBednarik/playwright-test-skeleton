dae15b1e6336e08cefb5e5909f5101426b5c3bd1
radek.Bednarik <radek.bednarik@tesena.com>
2024-04-04 16:02:53 +0200

    dev: use copySync to remove callback

--- bin/playwright-skeleton-ts.js
+++ bin/playwright-skeleton-ts.js
@@ -4,7 +4,7 @@
 const { join } = require("path");
 const { execSync } = require("child_process");
 const { platform } = require("os");
-const { copy } = require("fs-extra");
+const { copySync } = require("fs-extra");
 const chalk = require("chalk");
 
 const projectDir = process.cwd();
@@ -12,42 +12,33 @@ const templateDir = join(__dirname, "../templates/ts");
 
 console.log(chalk.blue("Creating skeleton project...\n"));
 
-copy(templateDir, projectDir, (err) => {
-  if (err)
-    throw Error(
-      chalk.red(
-        `Copying from ${templateDir} to ${projectDir} failed with error: ${err}`
-      )
-    );
+copySync(templateDir, projectDir);
 
-  const opts = { cwd: projectDir };
+const opts = { cwd: projectDir };
 
-  // init git repo with branch name as main
-  console.log(chalk.green("Setting up git repo..."));
-  execSync("git init", opts);
+// init git repo with branch name as main
+console.log(chalk.green("Setting up git repo..."));
+execSync("git init", opts);
 
-  // install deps
-  console.log(chalk.green("Running npm install..."));
-  execSync("npm install", opts);
+// install deps
+console.log(chalk.green("Running npm install..."));
+execSync("npm install", opts);
 
-  // create default precommit hooks
-  console.log(chalk.green("Setting up precommit hooks..."));
-  execSync("npx mrm@2 lint-staged", opts);
+// create default precommit hooks
+console.log(chalk.green("Setting up precommit hooks..."));
+execSync("npx mrm@2 lint-staged", opts);
 
-  // TODO: we have to modify package.json to set correct lint-staged props
+// TODO: we have to modify package.json to set correct lint-staged props
 
-  // finish playwright setup
-  if (platform() === "linux") {
-    console.log(
-      chalk.green(
-        "Linux platform detected, setting up needed linux packages..."
-      )
-    );
-    execSync("npx playwright install-deps", opts);
-  }
+// finish playwright setup
+if (platform() === "linux") {
+  console.log(
+    chalk.green("Linux platform detected, setting up needed linux packages...")
+  );
+  execSync("npx playwright install-deps", opts);
+}
 
-  console.log(chalk.green("Installing Playwright browser binaries..."));
-  execSync("npx playwright install", opts);
+console.log(chalk.green("Installing Playwright browser binaries..."));
+execSync("npx playwright install", opts);
 
-  console.log(chalk.blue("\nSkeleton project created"));
-});
+console.log(chalk.blue("\nSkeleton project created"));
