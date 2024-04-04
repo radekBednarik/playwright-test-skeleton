5a0fe39abf215f64778f3ae12e8d7bd977d08c5c
radek.Bednarik <radek.bednarik@tesena.com>
2024-04-04 10:46:56 +0200

    dev: _

--- bin/playwright-skeleton-ts.js
+++ bin/playwright-skeleton-ts.js
@@ -13,25 +13,25 @@ copy(templateDir, projectDir, (err) => {
     throw Error(
       `Copying from ${templateDir} to ${projectDir} failed with error: ${err}`
     );
+
+  const opts = { cwd: projectDir, stdio: "inherit" };
+
+  console.log("Creating skeleton project...");
+
+  // init git repo
+  execSync("git init && git branch -m main", opts);
+
+  // install deps
+  execSync("npm install", opts);
+  console.log("basic deps installed");
+
+  // create default precommit hooks
+  execSync("npx mrm@2 lint-staged", opts);
+  console.log("default precommit hooks created");
+
+  // finish playwright setup
+  if (platform === "linux") {
+    execSync("npx playwright install-deps", opts);
+  }
+  execSync("npx playwright install", opts);
 });
-
-const opts = { cwd: projectDir, stdio: "inherit" };
-
-console.log("Creating skeleton project...");
-
-// init git repo
-execSync("git init && git branch -m main", opts);
-
-// install deps
-execSync("npm install", opts);
-console.log("basic deps installed");
-
-// create default precommit hooks
-execSync("npx mrm@2 lint-staged", opts);
-console.log("default precommit hooks created");
-
-// finish playwright setup
-if (platform === "linux") {
-  execSync("npx playwright install-deps", opts);
-}
-execSync("npx playwright install", opts);
