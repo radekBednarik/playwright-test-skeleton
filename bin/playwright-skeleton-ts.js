349b4bf6e384062f7930d566d8a35a14c4b94bbe
radek.Bednarik <radek.bednarik@tesena.com>
2024-04-04 16:09:54 +0200

    dev: pass for adjusting precommit config in package.json

--- bin/playwright-skeleton-ts.js
+++ bin/playwright-skeleton-ts.js
@@ -4,17 +4,43 @@
 const { join } = require("path");
 const { execSync } = require("child_process");
 const { platform } = require("os");
-const { copySync } = require("fs-extra");
+const { copySync, readFileSync, writeFileSync } = require("fs-extra");
 const chalk = require("chalk");
 
 const projectDir = process.cwd();
 const templateDir = join(__dirname, "../templates/ts");
+const opts = { cwd: projectDir };
+
+const adjustPackage = () => {
+  function hasProp(obj, prop) {
+    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  const pJson = JSON.parse(
+    readFileSync(join(projectDir, "package.json"), { encoding: "utf-8" })
+  );
+
+  if (hasProp(pJson["lint-staged"], "*.js")) {
+    delete pJson["lint-staged"]["*.js"];
+  }
+
+  if (hasProp(pJson["lint-staged"], "*.{js,css,md}")) {
+    delete pJson["lint-staged"]["*.{js,css,md}"];
+  }
+
+  pJson["lint-staged"]["*.{js,ts}"] = ["eslint --cache", "prettier --write"];
+  pJson["lint-staged"]["*.{md,json}"] = "prettier --write";
+
+  writeFileSync(join(projectDir, "package.json"), JSON.stringify(pJson));
+};
 
 console.log(chalk.blue("Creating skeleton project...\n"));
 
 copySync(templateDir, projectDir);
-
-const opts = { cwd: projectDir };
 
 // init git repo with branch name as main
 console.log(chalk.green("Setting up git repo..."));
@@ -27,8 +53,8 @@ execSync("npm install", opts);
 // create default precommit hooks
 console.log(chalk.green("Setting up precommit hooks..."));
 execSync("npx mrm@2 lint-staged", opts);
-
-// TODO: we have to modify package.json to set correct lint-staged props
+// adjust package.json lint-staged prop to have needed values
+adjustPackage();
 
 // finish playwright setup
 if (platform() === "linux") {
